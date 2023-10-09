c4b2068d8efc2de7e0cf642ddab4f102b2799a76
LC <gingeraleisthebest@gmail.com>
2023-10-09 17:29:48 -0400

    moved read file to its own module

---
+++ util.js
@@ -0,0 +1,17 @@
+const fs = require('fs/promises');
+
+async function sendPage(filename) {
+  const errorfile = await fs.readFile('404.html', 'utf8');
+  try {
+    if (filename === '/') {
+      const data = await fs.readFile('index.html', 'utf8');
+      return data;
+    }
+    const data = await fs.readFile(filename.substring(1) + '.html', 'utf8');
+    return data;
+  } catch(err) {
+    return errorfile;
+  }
+}
+
+module.exports = {sendPage};
